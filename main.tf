0ed5dad765d4424f195f177b14caa77f994b04fc
renardeinside <polarpersonal@gmail.com>
2021-12-20 13:40:57 +0100

    add init impl

--- main.tf
+++ main.tf
@@ -11,6 +11,28 @@ variable "databricks_profile" {
 }
 
 provider "databricks" {
-    profile = vars.databricks_profile
+    profile = var.databricks_profile
 }
 
+locals {
+  raw_settings = yamldecode(file("${path.module}/permissions.yml"))
+  settings = tomap({
+      for p in local.raw_settings.principals: p.name => {
+          database = p.database
+          table = p.table 
+          privileges = tolist(p.privileges)
+      }
+  })
+}
+
+resource "databricks_sql_permissions" "dynamic_permissions" {
+    for_each = local.settings
+
+    database = each.value.database 
+    table = each.value.table 
+
+    privilege_assignments {
+        principal = each.key
+        privileges = each.value.privileges
+    }
+}
